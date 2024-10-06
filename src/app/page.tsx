4544d21ffeb6c74735604112cb7901a410757e0e
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-10-05 19:15:47 -0600

    Create a sendEmail function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 export default function HomePage() {
+
+  async function sendEmail(formData: FormData) {
+    'use server'
+
+    const title = formData.get('title')
+    const to = formData.get('to')
+    const content = formData.get('content')
+
+    if (!title || !to || !content) {
+      return alert('Missing required fields')
+    }
+
+    console.log({
+      title,
+      to,
+      content
+    })
+
+  }
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <form action="" className="flex flex-col gap-y-4">
-        <input type="text" name="" placeholder="Email Tittle" className="bg-slate-400 block p-2 placeholder:text-white"/>
-        <input type="email" name="" placeholder="To" className="bg-slate-400 block p-2 placeholder:text-white"/>
+      <form action={sendEmail} method="POST" className="flex flex-col gap-y-4">
+        <input type="text" name="title" placeholder="Email Tittle" className="bg-slate-400 block p-2 placeholder:text-white"/>
+        <input type="email" name="to" placeholder="To" className="bg-slate-400 block p-2 placeholder:text-white"/>
         <textarea name="content" className="bg-slate-400 block p-2 placeholder:text-white" placeholder="Write to email.."></textarea>
         <button className="bg-green-500 text-white p-2 rounded-md hover:bg-green-600">Send</button>
       </form>
