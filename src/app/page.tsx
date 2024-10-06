ef6832fabe2485e7f9c1314b3784100790ccbddd
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-10-05 18:37:25 -0600

    Refactor email form styling in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function HomePage() {
   return (
-    <div>
-      <form action="">
-        <input type="text" name="" placeholder="Email Tittle" />
-        <input type="email" name="" placeholder="To" />
-        <textarea name="content"></textarea>
-        <button>Send</button>
+    <div className="flex justify-center items-center h-screen">
+      <form action="" className="flex flex-col gap-y-4">
+        <input type="text" name="" placeholder="Email Tittle" className="bg-slate-400 block p-2 placeholder:text-white"/>
+        <input type="email" name="" placeholder="To" className="bg-slate-400 block p-2 placeholder:text-white"/>
+        <textarea name="content" className="bg-slate-400 block p-2 placeholder:text-white" placeholder="Write to email.."></textarea>
+        <button className="bg-green-500 text-white p-2 rounded-md hover:bg-green-600">Send</button>
       </form>
     </div>
   )
